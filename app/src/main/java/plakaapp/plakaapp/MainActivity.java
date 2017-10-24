3c8f7108a5b048a9604a87f0d2905ecea1ec91f5
cumen <ahmetcumen@gmail.com>
2017-10-25 02:29:35 +0300

    Giriş Ekranı Dizaynı yapıldı Case(15)

--- app/src/main/java/plakaapp/plakaapp/MainActivity.java
+++ app/src/main/java/plakaapp/plakaapp/MainActivity.java
@@ -2,12 +2,20 @@ package plakaapp.plakaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.graphics.Typeface;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_screen_activity);
+
+        //Logoya yazı fontu eklendi
+        TextView Logo = (TextView) findViewById(R.id.Logo);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/daysoneregular.ttf");
+        Logo.setTypeface(typeface);
+        //Logoya yazı fontu eklendi
     }
 }

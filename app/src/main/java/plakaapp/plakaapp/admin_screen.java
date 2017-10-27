b827666f667f7ad316938546fbd0c4caec0a9e5e
Berke D <tr.berkedundar@gmail.com>
2017-10-27 23:49:44 +0300

    (Case 49) Admin ana panelini tab'lar ile oluşturmak

--- app/src/main/java/plakaapp/plakaapp/admin_screen.java
+++ app/src/main/java/plakaapp/plakaapp/admin_screen.java
@@ -2,16 +2,32 @@ package plakaapp.plakaapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
 
 /**
  * Created by cumen on 26.10.2017.
  */
 
-public class admin_screen extends AppCompatActivity{
+public class admin_screen extends TabActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_screen_activity);
+
+        TabHost tabh = (TabHost)findViewById(android.R.id.tabhost);
+
+        TabSpec tab1 = tabh.newTabSpec("tab menü 1. seçenek");
+        TabSpec tab2 = tabh.newTabSpec("tab menü 2. seçenek");
+
+        tab1.setIndicator("1.sekme");
+        tab1.setContent(new Intent(this,tab_uyeler.class));
+        tab2.setIndicator("2.sekme");
+        tab2.setContent(new Intent(this,tab_uyeler.class));
+        tabh.addTab(tab1); tabh.addTab(tab2);
     }
 }
---
+++ app/src/main/java/plakaapp/plakaapp/tab_uyeler.java
@@ -0,0 +1,16 @@
+package plakaapp.plakaapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Simulakra on 27.10.2017.
+ */
+
+public class tab_uyeler extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab_uyeler);
+    }
+}

b383761d76ebd52d74375cdd35dc2ba075ee41ce
Berke D <tr.berkedundar@gmail.com>
2017-10-28 18:06:07 +0300

    (Case 14) pencere tasarımındaki düzenlemeler

--- app/src/main/java/plakaapp/plakaapp/admin_screen.java
+++ app/src/main/java/plakaapp/plakaapp/admin_screen.java
@@ -22,27 +22,27 @@ public class admin_screen extends TabActivity{
         TabHost tabh = (TabHost)findViewById(android.R.id.tabhost);
 
         TabSpec tab1 = tabh.newTabSpec("tab menü üyeler");
-        tab1.setIndicator("Üye");
+        tab1.setIndicator("Üyeler");
         tab1.setContent(new Intent(this,tab_uyeler.class));
         tabh.addTab(tab1);
 
         TabSpec tab2 = tabh.newTabSpec("tab menü sorular");
-        tab2.setIndicator("Soru");
+        tab2.setIndicator("Sorular");
         tab2.setContent(new Intent(this,tab_sorular.class));
         tabh.addTab(tab2);
 
-        TabSpec tab3 = tabh.newTabSpec("tab menü sorular");
-        tab3.setIndicator("Plaka");
+        TabSpec tab3 = tabh.newTabSpec("tab menü plakalar");
+        tab3.setIndicator("Plakalar");
         tab3.setContent(new Intent(this,tab_plakalar.class));
         tabh.addTab(tab3);
 
-        TabSpec tab4 = tabh.newTabSpec("tab menü sorular");
-        tab4.setIndicator("Cins");
+        TabSpec tab4 = tabh.newTabSpec("tab menü cinsler");
+        tab4.setIndicator("Cinsler");
         tab4.setContent(new Intent(this,tab_cinsler.class));
         tabh.addTab(tab4);
 
-        TabSpec tab5 = tabh.newTabSpec("tab menü sorular");
-        tab5.setIndicator("Tür");
+        TabSpec tab5 = tabh.newTabSpec("tab menü türler");
+        tab5.setIndicator("Türler");
         tab5.setContent(new Intent(this,tab_turler.class));
         tabh.addTab(tab5);
     }
--- app/src/main/java/plakaapp/plakaapp/tab_uyeler.java
+++ app/src/main/java/plakaapp/plakaapp/tab_uyeler.java
@@ -1,7 +1,11 @@
 package plakaapp.plakaapp;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by Simulakra on 27.10.2017.
@@ -12,5 +16,20 @@ public class tab_uyeler extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tab_uyeler);
+
+        Button bttt= (Button) findViewById(R.id.bt_ekle);
+        bttt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProgressDialog progressDialog= ProgressDialog.show(tab_uyeler.this, "Ekleme İşlemi",
+                        "Loading. Please wait...", true);
+
+                progressDialog.dismiss();
+                Toast.makeText(getApplicationContext(), "Ekleme İşlemi Tamamlandı", Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+
+
 }

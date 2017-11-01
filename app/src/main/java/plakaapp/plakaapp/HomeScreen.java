665cee8c3cca5c9ba9cd3885ac5a32af046203e9
cumen <ahmetcumen@gmail.com>
2017-11-01 14:24:52 +0300

    Tasarım çözünürlük sorunu giderildi

--- app/src/main/java/plakaapp/plakaapp/HomeScreen.java
+++ app/src/main/java/plakaapp/plakaapp/HomeScreen.java
@@ -1,8 +1,14 @@
 package plakaapp.plakaapp;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -24,7 +30,40 @@ public class HomeScreen extends AppCompatActivity {
         String kuladi = intent.getStringExtra("K_Adi");//kullanıcı adı
         String id = intent.getStringExtra("ID"); //kullanıcı idsi
 
-        tvKulAdi.setText(kuladi);
+
+
+
 
     }
+
+    //menu
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText("asd");
+                    return true;
+                case R.id.navigation_list:
+                    mTextMessage.setText("2");
+                    return true;
+                case R.id.navigation_report:
+                    mTextMessage.setText("3");
+                    return true;
+                case R.id.navigation_activity:
+                    mTextMessage.setText("4");
+                    return true;
+                case R.id.navigation_about:
+                    mTextMessage.setText("4");
+                    return true;
+            }
+            return false;
+        }
+
+    };
+    //menu
 }

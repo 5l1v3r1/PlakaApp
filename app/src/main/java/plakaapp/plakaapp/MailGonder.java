b1e73af0f3c4940cf204735eb620f552eb74852b
cumen <ahmetcumen@gmail.com>
2017-11-21 17:05:56 +0300

    Mail gonderme sınıfı oluşturuldu.(Case 68)

---
+++ app/src/main/java/plakaapp/plakaapp/MailGonder.java
@@ -0,0 +1,28 @@
+package plakaapp.plakaapp;
+
+import android.app.Activity;
+import android.content.Intent;
+
+/**
+ * Created by cumen on 21.11.2017.
+ */
+
+public class MailGonder extends Activity {
+
+    public static boolean mgonder(String baslik, String icerik , String mailler[], Activity act){
+
+        try {
+            Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+            emailIntent.setType("plain/text");
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, baslik);//Email konusu
+            emailIntent.putExtra(Intent.EXTRA_TEXT, icerik);//Email içeriği
+            act.startActivity(Intent.createChooser(emailIntent, "E-mail Göndermek için Seçiniz:")); //birden fazla email uygulaması varsa seçmek için
+            emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, mailler);
+            act.startActivity(emailIntent);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+}

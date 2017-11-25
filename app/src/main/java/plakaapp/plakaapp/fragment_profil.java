27ddb40436c3a1c3eda9914e548d10bf92248840
Berke D <tr.berkedundar@gmail.com>
2017-11-25 18:42:39 +0200

    merge for last thing

--- app/src/main/java/plakaapp/plakaapp/fragment_profil.java
+++ app/src/main/java/plakaapp/plakaapp/fragment_profil.java
@@ -1,5 +1,6 @@
 package plakaapp.plakaapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,13 +12,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -28,16 +33,19 @@ import java.util.concurrent.ExecutionException;
  */
 
 public class fragment_profil extends Fragment {
+    View vvv;
     String K_ID, K_Ad;
-    Boolean e1=false,e2=false,e3=false,e4=false,e5=false;
+    public Boolean e1, e2 , e3 , e4 , e5;
     public JSONArray uyeler, sorular;
+
     public fragment_profil() {
 
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.fragment_profil, container, false);
-
+        View view = inflater.inflate(R.layout.fragment_profil, container, false);
+        vvv = view;
         Intent intent = getActivity().getIntent();
         K_Ad = intent.getStringExtra("K_Adi");//kullanıcı adı
         K_ID = intent.getStringExtra("ID"); //kullanıcı id
@@ -45,14 +53,96 @@ public class fragment_profil extends Fragment {
         SorularSpinnerDoldur(view);
         UyeBilgiDoldur(view);
         CreateControlListener(view);
-        e1=e2=e3=e4=e5=false;
+        e1 = e2 = e3 = e4 = e5 = false;
         CreateButtonListener(view);
 
         return view;
     }
 
-    private void CreateButtonListener(View view) {
+    private void CreateButtonListener(final View view) {
+        Button btn = (Button) view.findViewById(R.id.et_profil_guncelle);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!e1 & !e2 & !e3 & !e4 & !e5) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                    builder.setMessage("Hiçbir değeri değiştirmediniz")
+                            .setNegativeButton("Tamam", null).create().show();
+                    return;
+                }
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case DialogInterface.BUTTON_POSITIVE:
+                                //Yes button clicked
+                                try {
+                                    int index = 0;
+                                    for (int i = 0; i < uyeler.length(); i++) {
+                                        if (uyeler.getJSONObject(i).getJSONObject("message").getString("ID") == K_ID) {
+                                            index = i;
+                                            break;
+                                        }
+                                    }
+                                    JSONObject jsonChildNode = uyeler.getJSONObject(index).getJSONObject("message");
+                                    EditText k_adi = (EditText) vvv.findViewById(R.id.et_profil_kuladi);
+                                    EditText sifre = (EditText) vvv.findViewById(R.id.et_profil_email);
+                                    EditText eposta = (EditText) vvv.findViewById(R.id.et_profil_parola);
+                                    EditText rep = (EditText) vvv.findViewById(R.id.et_profil_gcevap);
 
+                                    String v1, v2, v3, v4, v5;
+
+                                    if (e1) v1 = k_adi.getText().toString();
+                                    else v1 = jsonChildNode.getString("K_Adi");
+                                    if (e2) v2 = eposta.getText().toString();
+                                    else v2 = jsonChildNode.getString("K_Mail");
+                                    if (e3) v3 = sifre.getText().toString();
+                                    else v3 = "~~";
+                                    if (e4) v4 = SpinnerPull(view);
+                                    else v4 = jsonChildNode.getString("K_Soru");
+                                    if (e5) v5 = rep.getText().toString();
+                                    else v5 = jsonChildNode.getString("K_Cevap");
+
+                                    JSONObject temp = new JSONObject(new JSONtask().execute(
+                                            Config.Kguncelle_URL(jsonChildNode.getString("ID"),
+                                                    jsonChildNode.getString("Admin"),
+                                                    URLEncoder.encode(v1, "utf-8"),
+                                                    URLEncoder.encode(v3, "utf-8"),
+                                                    jsonChildNode.getString("K_Rep"),
+                                                    URLEncoder.encode(v2, "utf-8"),
+                                                    URLEncoder.encode(v4, "utf-8"),
+                                                    URLEncoder.encode(v5, "utf-8"))).get());
+                                    if (JsonErrorCheck(temp)) {
+                                        Toast.makeText(getContext(), "Güncelleme İşlemi Tamamlandı", Toast.LENGTH_LONG).show();
+                                        UyeBilgiDoldur(vvv);
+                                        return;
+                                    }
+                                } catch (Exception e) {
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                                    builder.setMessage(e.getMessage())
+                                            .setNegativeButton("Tamam", null).create().show();
+                                }
+                                break;
+
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                //No button clicked
+                                break;
+                        }
+                    }
+                };
+                String mesaj = "Değiştirilecek Bilgiler;\n";
+                if (e1) mesaj += "Kullanıcı Adı,\n";
+                if (e2) mesaj += "Mail,\n";
+                if (e3) mesaj += "Parola,\n";
+                if (e4) mesaj += "Güvenlik Sorusu,\n";
+                if (e5) mesaj += "Soru Cevabı,\n";
+                mesaj += "\nBu bilgileri değiştirmek istediğinze emin misiniz?";
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setMessage(mesaj).setPositiveButton("Evet", dialogClickListener)
+                        .setNegativeButton("Hayır", dialogClickListener).show();
+
+            }
+        });
     }
 
     private void CreateControlListener(View view) {
@@ -69,7 +159,7 @@ public class fragment_profil extends Fragment {
 
             @Override
             public void afterTextChanged(Editable s) {
-                e1=true;
+                e1 = true;
             }
         });
         ((EditText) view.findViewById(R.id.et_profil_email)).addTextChangedListener(new TextWatcher() {
@@ -85,7 +175,7 @@ public class fragment_profil extends Fragment {
 
             @Override
             public void afterTextChanged(Editable s) {
-                e2=true;
+                e2 = true;
             }
         });
         ((EditText) view.findViewById(R.id.et_profil_parola)).addTextChangedListener(new TextWatcher() {
@@ -101,7 +191,7 @@ public class fragment_profil extends Fragment {
 
             @Override
             public void afterTextChanged(Editable s) {
-                e3=true;
+                e3 = true;
             }
         });
         ((EditText) view.findViewById(R.id.et_profil_gcevap)).addTextChangedListener(new TextWatcher() {
@@ -117,13 +207,13 @@ public class fragment_profil extends Fragment {
 
             @Override
             public void afterTextChanged(Editable s) {
-                e5=true;
+                e5 = true;
             }
         });
         ((Spinner) view.findViewById(R.id.sp_profil_gsorusu)).setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                e4=true;
+                e4 = true;
             }
 
             @Override
@@ -136,7 +226,7 @@ public class fragment_profil extends Fragment {
 
     private void UyeBilgiDoldur(View view) {
         try {
-            uyeler= new JSONArray(new JSONtask().execute(Config.KLISTELE_URL).get());
+            uyeler = new JSONArray(new JSONtask().execute(Config.KLISTELE_URL).get());
             for (int i = 0; i < uyeler.length(); i++) {
                 JSONObject jsonSoru = uyeler.getJSONObject(i).getJSONObject("message");
 
@@ -146,6 +236,7 @@ public class fragment_profil extends Fragment {
                 ((EditText) view.findViewById(R.id.et_profil_gcevap)).setText(jsonSoru.getString("K_Cevap"));
                 SpinnerPush(jsonSoru.getString("K_Soru"), view);
             }
+            e1 = e2 = e3 = e4 = e5 = false;
         } catch (Exception e) {
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setMessage(e.getMessage())
@@ -155,38 +246,52 @@ public class fragment_profil extends Fragment {
         }
     }
 
-    private void SpinnerPush(String ID,View view) {
-        try{
-            if(ID.isEmpty()) return;
+    private String SpinnerPull(View view) {
+        Spinner soru = ((Spinner) view.findViewById(R.id.sp_profil_gsorusu));
+            String index = "0";
+            try {
+                for (int i=0;i<sorular.length();i++){
+
+                    if (soru.getSelectedItem().toString().equals(sorular.getJSONObject(i).getJSONObject("message").getString("SoruMetin"))) {
+                        index = sorular.getJSONObject(i).getJSONObject("message").getString("ID");
+                        return index;
+                    }
+                }
+            }catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return index;
+    }
+
+    private void SpinnerPush(String ID, View view) {
+        try {
+            if (ID.isEmpty()) return;
 
             Spinner soru = ((Spinner) view.findViewById(R.id.sp_profil_gsorusu));
 
-            int arrayPos=0;
-            for (int i=0;i<sorular.length();i++)
-            {
-                if(sorular.getJSONObject(i).getJSONObject("message").getString("ID")==ID)
-                {
-                    arrayPos=i;
+            int arrayPos = 0;
+            for (int i = 0; i < sorular.length(); i++) {
+                if (sorular.getJSONObject(i).getJSONObject("message").getString("ID") == ID) {
+                    arrayPos = i;
                     break;
                 }
             }
 
-            for (int i=0;i<soru.getCount();i++)
-            {
-                if(soru.getItemAtPosition(i).toString()==
-                        sorular.getJSONObject(arrayPos).getJSONObject("message").getString("SoruMetin"))
-                {
+            for (int i = 0; i < soru.getCount(); i++) {
+                if (soru.getItemAtPosition(i).toString() ==
+                        sorular.getJSONObject(arrayPos).getJSONObject("message").getString("SoruMetin")) {
                     soru.setSelection(i);
                     return;
                 }
             }
-        }catch (Exception e) {
+        } catch (Exception e) {
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setMessage(e.getMessage())
                     .setNegativeButton("Tamam", null)
                     .create()
                     .show();
         }
+        e4=false;
     }
 
     public void SorularSpinnerDoldur(View view) {
@@ -198,7 +303,7 @@ public class fragment_profil extends Fragment {
             for (int i = 0; i < sorular.length(); i++) {
                 JSONObject jsonSoru = sorular.getJSONObject(i);
 
-                String soruMetni=jsonSoru.getJSONObject("message").getString("SoruMetin");
+                String soruMetni = jsonSoru.getJSONObject("message").getString("SoruMetin");
                 result.add(soruMetni);
             }
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, result);
@@ -212,5 +317,26 @@ public class fragment_profil extends Fragment {
                     .show();
         }
 
+    }
+
+    private boolean JsonErrorCheck(JSONObject temp) {
+        try {
+            //JSONObject js1 =temp.getJSONObject(0);
+            JSONObject js2 = temp.getJSONObject("message");
+            String durum = js2.getString("durum");
+            if (durum.equals(String.valueOf("basarili")))
+                return true;
+            if (durum.equals(String.valueOf("99")))
+                Toast.makeText(getContext(), "SQL Hatası alındı", Toast.LENGTH_LONG).show();
+            if (durum.equals(String.valueOf("8")))
+                Toast.makeText(getContext(), "Kayıt Bulunamadı", Toast.LENGTH_LONG).show();
+            if (durum.equals(String.valueOf("1")))
+                Toast.makeText(getContext(), "Kayıt Mevcut", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(e.getMessage())
+                    .setNegativeButton("Tamam", null).create().show();
+        }
+        return false;
     }
 }
